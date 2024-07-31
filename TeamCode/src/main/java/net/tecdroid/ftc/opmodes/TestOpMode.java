8332d7c4e1573b398d7e0b6409487d2f1a45cbcd
Ayimany <mooncatayimany@proton.me>
2024-07-31 00:51:25 -0600

    Add Testing OpMode

--- TeamCode/src/main/java/net/tecdroid/ftc/opmodes/TestOpMode.java
+++ TeamCode/src/main/java/net/tecdroid/ftc/opmodes/TestOpMode.java
@@ -1,4 +1,41 @@
 package net.tecdroid.ftc.opmodes;
 
-public class TestOpMode {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import net.tecdroid.ftc.standards.SensorStandards;
+import net.tecdroid.ftc.subsystems.SubsystemCollection;
+import net.tecdroid.ftc.subsystems.drivetrain.MecanumDrivetrain;
+import net.tecdroid.ftc.util.MathUtil;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.joml.Vector2d;
+
+public class TestOpMode extends OpMode {
+
+    private MecanumDrivetrain drivetrain = null;
+    private IMU imu = null;
+
+    @Override
+    public void init() {
+        drivetrain = new MecanumDrivetrain(hardwareMap, telemetry);
+        imu = hardwareMap.get(IMU.class, SensorStandards.IMU_NAME);
+    }
+
+    @Override
+    public void loop() {
+        SubsystemCollection.logSubsystems();
+
+        final Vector2d linearVelocity = new Vector2d(
+                -gamepad1.left_stick_y,
+                -gamepad1.left_stick_x
+        );
+
+        final double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        final Vector2d rotatedVelocity = MathUtil.rotateVector(linearVelocity, -heading);
+
+        final double angularVelocity = -gamepad1.right_stick_x;
+
+        drivetrain.drive(rotatedVelocity, angularVelocity);
+    }
 }
