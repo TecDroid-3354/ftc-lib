c9d5491cf4bf0f5e10d405334027faf08c359ae8
Ayimany <mooncatayimany@proton.me>
2024-07-31 12:32:48 -0600

    make `MathUtil#rotateVector` `static` again

--- TeamCode/src/main/java/net/tecdroid/ftc/util/MathUtil.java
+++ TeamCode/src/main/java/net/tecdroid/ftc/util/MathUtil.java
@@ -6,7 +6,7 @@ import org.joml.Vector2d;
 public class MathUtil {
 
     // For the curious: https://en.wikipedia.org/wiki/Rotation_matrix
-    public Vector2d rotateVector(Vector2d vector, double angleRadians) {
+    public static Vector2d rotateVector(Vector2d vector, double angleRadians) {
         return new Vector2d(vector).mul(new Matrix2d(
                 Math.cos(angleRadians), -Math.sin(angleRadians),
                 Math.sin(angleRadians), Math.cos(angleRadians)
