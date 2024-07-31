b8217aadd8cb750904b917b023cab50cf9c2babf
Ayimany <mooncatayimany@proton.me>
2024-07-31 12:17:14 -0600

    MathUtil -> UtilityClass

--- TeamCode/src/main/java/net/tecdroid/ftc/util/MathUtil.java
+++ TeamCode/src/main/java/net/tecdroid/ftc/util/MathUtil.java
@@ -3,16 +3,17 @@ package net.tecdroid.ftc.util;
 import org.joml.Matrix2d;
 import org.joml.Vector2d;
 
+import lombok.experimental.UtilityClass;
+import lombok.val;
+
+@UtilityClass
 public class MathUtil {
 
     // For the curious: https://en.wikipedia.org/wiki/Rotation_matrix
-    public static Vector2d rotateVector(Vector2d vector, double angleRadians) {
-        Vector2d result = new Vector2d(vector);
-        result.mul(new Matrix2d(
+    public Vector2d rotateVector(Vector2d vector, double angleRadians) {
+        return new Vector2d(vector).mul(new Matrix2d(
                 Math.cos(angleRadians), -Math.sin(angleRadians),
                 Math.sin(angleRadians), Math.cos(angleRadians)
         ));
-
-        return result;
     }
 }
